63998209cdbad40fa4bbe0b618c9056f8b47e7e6
GaboRenovation <85262188+GaboRenovation@users.noreply.github.com>
2021-10-06 06:39:25 -0500

    Delete config.js

--- Simple_Map/static/js/config.js
+++
@@ -1,8 +0,0 @@
-
-// Default PUBLIC token
-// const API_KEY = "pk.eyJ1IjoiZ2Fib3Jlbm92YXRpb24iLCJhIjoiY2t1Ymc5dW9zMDZwNTJwbzk2bG82bTFsbSJ9.crd1af6Il4z2h-tEtVgohA"
-
-
-// mapbox-bootcamp public token
-const API_KEY = "pk.eyJ1IjoiZ2Fib3Jlbm92YXRpb24iLCJhIjoiY2t1ZmQ0YnkyMXRvdjJ3bWFrOTFvazF4OCJ9.V-4JnkLuPk9XT2NldHcN0w"
-
