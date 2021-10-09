06eed47e51b1c3cf55bb094bd03dd66f79c8b4bb
GaboRenovation <gabo_renovation@infinitummail.com>
2021-10-09 14:25:10 -0500

    updating

--- Mapping_GeoJSON_MultipleMaps (copy)/static/js/logic.js
+++ Mapping_GeoJSON_MultipleMaps (copy)/static/js/logic.js
@@ -19,10 +19,17 @@
             accessToken: API_KEY
         });
 
+        let satelite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+          attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+              maxZoom: 18,
+              accessToken: API_KEY
+          });
+
 // Create a BASE LAYER that holds both maps.
 let baseMaps = {
   Street: streets,
-  Dark: dark
+  Dark: dark,
+  Satelite: satelite
 };
 
 // Create the map object with center, zoom level and default layer.
