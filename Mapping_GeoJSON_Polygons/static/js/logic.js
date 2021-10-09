811ae675420563264331afc695c3d59a52452664
GaboRenovation <gabo_renovation@infinitummail.com>
2021-10-09 15:07:41 -0500

    updating

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -211,7 +211,7 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
         accessToken: API_KEY
     });
 
-let satelite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let sateliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
@@ -219,16 +219,16 @@ let satelite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-st
 
 // Create a BASE LAYER that holds both maps.
 let baseMaps = {
-Street: streets,
-Dark: dark,
-Satelite: satelite
+"Streets": streets,
+"Dark": dark,
+"Satelite Streets": sateliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 1,
-    layers: [streets]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [sateliteStreets]
   })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -242,14 +242,17 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 // let airportData = "https://raw.githubusercontent.com/GaboRenovation/Mapping_Earthquakes/main/majorAirports.json";
 
-// let airportData = "majorAirports.json";
-let torontoData = "torontoRoutes.json";
+// let airportDat = "majorAirports.json";
+// let torontoData = "torontoRoutes.json";
+let torontoNeighborhoods = "torontoNeighborhoods.json";
 
 // // Accessing the Toronto airline routes GeoJSON URL.
-// let torontoData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoRoutes.json";
+// let torontoData = "https://raw.githubusercontent.com/    <GitHub_name>   /Mapping_Earthquakes/main/torontoRoutes.json";
+// Accessing the Toronto neighborhoods GeoJSON URL.
+// let torontoHoods = "https://raw.githubusercontent.com/   <GitHub_name>   /Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoNeighborhoods).then(function(data) {
     console.log(data);
 
   // Creating a GeoJSON layer with the retrieved data.
@@ -257,7 +260,7 @@ d3.json(torontoData).then(function(data) {
     color: "#ffffa1", 
     weight: 1,
     onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+        layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
     }
 })
 .addTo(map);
