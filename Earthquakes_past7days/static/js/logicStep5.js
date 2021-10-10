92a7e88c079521efcfebbcd20e9b4d43833a02f8
GaboRenovation <gabo_renovation@infinitummail.com>
2021-10-09 20:56:57 -0500

    updating

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -120,6 +120,7 @@ function getRadius(magnitude) {
 // 13-6-2 Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
 
+
     // We turn each feature into a circleMarker on the map.
         pointToLayer: function(feature, latlng) {
                 console.log(data);
@@ -137,31 +138,4 @@ L.geoJson(data, {
       earthquakes.addTo(map);
     });
 
-// Create a legend control object.
-// let legend = L.control({
-//   position: "bottomright"
-// });
 
-// Then add all the details for the legend.
-legend.onAdd = function() {
-  let div = L.DomUtil.create("div", "info legend");
-  const magnitudes = [0, 1, 2, 3, 4, 5];
-  const colors = [
-  "#98ee00",
-  "#d4ee00",
-  "#eecc00",
-  "#ee9c00",
-  "#ea822c",
-  "#ea2c2c"
-];
-// Looping through our intervals to generate a label with a colored square for each interval.
-for (var i = 0; i < magnitudes.length; i++) {
-  console.log(colors[i]);
-  div.innerHTML +=
-    "<i style='background: " + colors[i] + "'></i> " +
-    magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-}
-return div;
-// };
-legend.addTo(map);
-};
